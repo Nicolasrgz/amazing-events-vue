ddbb5a50e6996ea6f7735828a14822db1643565d
chaquito7 <nicolsgonza765@gmail.com>
2023-05-19 19:10:01 -0300

    version VueJs 1.3

--- js/index.js
+++ js/index.js
@@ -21,7 +21,7 @@ const app = createApp({
 }).catch(error => console.error(error))
     },
 
-    methods:{
+    computed:{
         filtrarPorTitulo (){
             this.filtroTitulo = this.apiEvents.filter(event => event.name.toLowerCase().includes(this.busqueda.toLowerCase()))
             console.log(this.filtroTitulo)
@@ -30,3 +30,72 @@ const app = createApp({
 })
 
 app.mount('#app')
+
+
+// let apiEvents
+// fetch("https://mindhub-xj03.onrender.com/api/amazing")
+// .then(res => res.json())
+// .then(data => {
+//   apiEvents = data
+//   imprimirDatos(apiEvents.events , section)
+//   imprimirCheckbox(apiEvents.events, formulario)
+// })
+
+// inputBusqueda.addEventListener("input", ()=>{
+//   let eventosFiltrados = apiEvents.events; 
+//     if (inputBusqueda.value) { 
+//       eventosFiltrados = filtrarPorTitulo(apiEvents.events, inputBusqueda.value);
+//     }
+//   let checkedCheckbox = formulario.querySelectorAll("input[name='checkbox']:checked");
+//     if (checkedCheckbox.length > 0) { 
+//       checkedCheckbox.forEach(checkbox => {
+//       eventosFiltrados = filtrarPorCategoria(eventosFiltrados, checkbox.value);
+//     });
+//   }
+//   section.innerHTML = "";
+//   imprimirDatos(eventosFiltrados, section);
+// });
+
+// formulario.addEventListener('change', ()=>{
+//   let eventosFiltrados = filtrarPorTitulo(apiEvents.events, inputBusqueda.value);
+//   let checkedCheckbox = formulario.querySelectorAll("input[name='checkbox']:checked");
+//   if (checkedCheckbox.length === 0) {
+//     section.innerHTML = "";
+//     imprimirDatos(eventosFiltrados, section);
+//   } else {
+//     checkedCheckbox.forEach(checkbox => {
+//       eventosFiltrados = filtrarPorCategoria(eventosFiltrados, checkbox.value);
+//     });
+//     section.innerHTML = "";
+//     imprimirDatos(eventosFiltrados, section);
+//   }
+// });
+
+
+
+// function armarCheckbocx(objeto) {
+//   return `<input type="checkbox" id="${objeto.category}" name="checkbox" value="${objeto.category}">
+//   <label for="${objeto.category}">${objeto.category}</label>`;
+// }
+
+// function imprimirCheckbox(lista, ubicacion) {
+//   let añadirCategoria = [];
+//   for (let elemento of lista){
+//     if (!añadirCategoria.includes(elemento.category)) {
+//       ubicacion.innerHTML += armarCheckbocx(elemento);
+//       añadirCategoria.push(elemento.category);
+//     }
+//   }
+// }
+
+// function filtrarPorTitulo (data, busqueda){
+//   return data.filter(event => event.name.toLowerCase().includes(busqueda.toLowerCase()))
+// }
+
+// function filtrarPorCategoria(data) {
+//   let categoriasSeleccionadas = [];
+//   formulario.querySelectorAll("input[name='checkbox']:checked").forEach((checkbox) => {
+//     categoriasSeleccionadas.push(checkbox.value);
+//   });
+//   return data.filter(event => categoriasSeleccionadas.includes(event.category));
+// }
