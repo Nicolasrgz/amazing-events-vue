c8a7f1cf103c6f66e49d80eab8e44b2d4a9b6174
chaquito7 <nicolsgonza765@gmail.com>
2023-05-19 14:16:20 -0300

    version VueJs 1.2

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
-alert("hokla")
+const {createApp} = Vue
+
+const app = createApp({
+
+    data(){
+        return{
+            apiEvents : [],
+            busqueda: "",
+            filtroTitulo: [],
+        }
+    },
+
+    created(){
+
+    const url = "https://mindhub-xj03.onrender.com/api/amazing"
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+    this.apiEvents = data.events 
+    
+}).catch(error => console.error(error))
+    },
+
+    methods:{
+        filtrarPorTitulo (){
+            this.filtroTitulo = this.apiEvents.filter(event => event.name.toLowerCase().includes(this.busqueda.toLowerCase()))
+            console.log(this.filtroTitulo)
+          }
+    }
+})
+
+app.mount('#app')
