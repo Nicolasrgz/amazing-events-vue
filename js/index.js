df9c011ef5830b132a826170f093d51447325174
chaquito7 <nicolsgonza765@gmail.com>
2023-05-20 12:04:04 -0300

    version VueJs 1.3.2

--- js/index.js
+++ js/index.js
@@ -7,8 +7,9 @@ const app = createApp({
             apiEvents : [],
             busqueda: "",
             filtroTitulo: [],
+            checkedInput: [],
             categorias: [],
-            newCategorys:[]
+            añadirCategoria: []
         }
     },
 
@@ -19,15 +20,14 @@ const app = createApp({
     .then(res => res.json())
     .then(data => {
     this.apiEvents = data.events 
-    this.categorias = this.apiEvents.map((event) => event.category)
-    this.newCategorys = [... new Set (this.categorias)] 
-     
+    this.categorias = this.apiEvents.map(event => event.category)
+    this.añadirCategoria = [... new Set (this.categorias)]
     
 }).catch(error => console.error(error))
     },
 
     method:{
-
+        
     },
 
 
@@ -36,12 +36,35 @@ const app = createApp({
             this.filtroTitulo = this.apiEvents.filter(event => event.name.toLowerCase().includes(this.busqueda.toLowerCase()))
             console.log(this.filtroTitulo)
           },
-          
+        //   filtroCategorias(){
+        //     this.añadirCategoria = []
+        //     for(let elementos of this.apiEvents){
+        //      if (!this.añadirCategoria.includes(elementos.category)) {
+        //              this.añadirCategoria.push(elementos.category);
+        //              console.log(this.añadirCategoria)
+        //          }
+        //     }
+        //  }  
     }
 })
 
 app.mount('#app')
 
+
+// function armarCheckbocx(objeto) {
+//   return `<input type="checkbox" id="${objeto.category}" name="checkbox" value="${objeto.category}">
+//   <label for="${objeto.category}">${objeto.category}</label>`;
+// }
+
+// function imprimirCheckbox(lista, ubicacion) {
+//   let añadirCategoria = [];
+//   for (let elemento of lista){
+//     if (!añadirCategoria.includes(elemento.category)) {
+//       ubicacion.innerHTML += armarCheckbocx(elemento);
+//       añadirCategoria.push(elemento.category);
+//     }
+//   }
+// }
 
 // let apiEvents
 // fetch("https://mindhub-xj03.onrender.com/api/amazing")
@@ -84,20 +107,7 @@ app.mount('#app')
 
 
 
-// function armarCheckbocx(objeto) {
-//   return `<input type="checkbox" id="${objeto.category}" name="checkbox" value="${objeto.category}">
-//   <label for="${objeto.category}">${objeto.category}</label>`;
-// }
 
-// function imprimirCheckbox(lista, ubicacion) {
-//   let añadirCategoria = [];
-//   for (let elemento of lista){
-//     if (!añadirCategoria.includes(elemento.category)) {
-//       ubicacion.innerHTML += armarCheckbocx(elemento);
-//       añadirCategoria.push(elemento.category);
-//     }
-//   }
-// }
 
 // function filtrarPorTitulo (data, busqueda){
 //   return data.filter(event => event.name.toLowerCase().includes(busqueda.toLowerCase()))
