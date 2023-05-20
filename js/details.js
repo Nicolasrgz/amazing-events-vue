58a8fe1cb7770e8f5e0997c911bce8c46b1618fe
chaquito7 <nicolsgonza765@gmail.com>
2023-05-20 18:08:38 -0300

    version Vue Js 1.3.4

--- js/details.js
+++ js/details.js
@@ -1,1 +1,30 @@
-alert("hokla")
+const { createApp } = Vue
+
+const app = createApp({
+  data() {
+    return {
+      eventDetails: null,
+      apiEvents: [],
+      params: {}
+    }
+  },
+
+  created() {
+    this.params = new URLSearchParams(location.search)
+    const identificador = this.params.get('id')
+    console.log(identificador)
+    console.log(this.params)
+    fetch("https://mindhub-xj03.onrender.com/api/amazing")
+      .then(res => res.json())
+      .then(data => {
+        this.apiEvents = data.events
+        this.eventDetails = this.apiEvents.find(event => event._id == identificador)
+        document.title = `details of ${this.eventDetails.name}`
+      })
+      .catch(error => console.error(error))
+  }
+})
+
+app.mount('#app')
+
+
